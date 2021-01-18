5764dc98514aa908859dfadbb1390b9409e10f2b
Rebecca Chen <rebecca061996@gmail.com>
2021-01-18 22:23:31 +0000

    added route for redirecting user to the longURL using the shortURL created

--- express_server.js
+++ express_server.js
@@ -16,27 +16,33 @@ const generateRandomString = function() {
   return Math.random().toString(36).substring(2, 8);
 };
 
+// homepage (root)
 app.get('/', (req, res) => {
   res.send('Hello!');
 });
 
+// urlDatabase
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
 
+// demonstrates can use HTML to display message
 app.get('/hello', (req, res) => {
   res.send('<html><body>Hello <b>World</b></body></html>\n');
 });
 
+// shows the shortURL longURL pairs
 app.get('/urls', (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
 });
 
+// for creating new shortURLs
 app.get('/urls/new', (req, res) => {
   res.render('urls_new');
 });
 
+// creates the shortURL and redirects to show user their newly created link
 app.post('/urls', (req, res) => {
   console.log(req.body); // logs the POST request body to console
   const shortURL = generateRandomString();
@@ -44,9 +50,16 @@ app.post('/urls', (req, res) => {
   res.redirect(`/urls/${shortURL}`);
 });
 
+// shows user their shortURL
 app.get('/urls/:shortURL', (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]}
   res.render('urls_show', templateVars);
+});
+
+// uses shortURL to redirect to longURL
+app.get('/u/:shortURL', (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
 });
 
 app.listen(PORT, () => {
