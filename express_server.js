33e55f012ce0406f0d7e53632fb845c959de6e06
Rebecca Chen <rebecca061996@gmail.com>
2021-01-18 20:00:42 +0000

    added a route for displaying a single URL and its shortened form

--- express_server.js
+++ express_server.js
@@ -9,7 +9,7 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   res.send('Hello!');
 });
 
@@ -24,6 +24,11 @@ app.get('/hello', (req, res) => {
 app.get('/urls', (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
+});
+
+app.get('/urls/:shortURL', (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]}
+  res.render('urls_show', templateVars);
 });
 
 app.listen(PORT, () => {
