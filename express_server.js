d76d0d47ec9428bf4f9cd79f7a5440f87f913e6e
Rebecca Chen <rebecca061996@gmail.com>
2021-01-19 17:51:28 +0000

    changed double quotes to single to be more consistent with rest of code

--- express_server.js
+++ express_server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require('body-parser');
 
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
