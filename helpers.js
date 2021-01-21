d164f3294e8d780aa0f5c71739f6cd5b0c9bd880
Rebecca Chen <rebecca061996@gmail.com>
2021-01-21 01:02:19 +0000

    moved 2 helper functions to helpers module

--- helpers.js
+++ helpers.js
@@ -6,4 +6,19 @@ const getUserByEmail = function(database, email) {
   }
 };
 
-module.exports = { getUserByEmail };
+// simulate generating unique shortURL - 6 random alphanumeric characters
+const generateRandomString = function() {
+  return Math.random().toString(36).substring(2, 8);
+};
+
+const urlsForUser = function(id) {
+  const userURLs = {};
+  for (let url in urlDatabase) {
+    if (urlDatabase[url].userID === id) {
+      userURLs[url] = urlDatabase[url];
+    }
+  }
+  return userURLs;
+};
+
+module.exports = { getUserByEmail, generateRandomString, urlsForUser };
